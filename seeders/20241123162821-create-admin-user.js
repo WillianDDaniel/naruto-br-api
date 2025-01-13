25818e863d337b794c55639008e78a507ee5781d
WillianDDaniel <williandeivitidaniel@live.com>
2025-01-13 00:46:40 -0300

    feat: added controllers actions to check 2fa

--- seeders/20241123162821-create-admin-user.js
+++ seeders/20241123162821-create-admin-user.js
@@ -5,10 +5,10 @@ require('dotenv').config();
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    const { USERNAME, USER_PASSWORD } = process.env;
+    const { USERNAME, USER_PASSWORD, USER_EMAIL } = process.env;
 
-    if (!USERNAME || !USER_PASSWORD) {
-      console.error('Please define USERNAME and USER_PASSWORD in the .env file');
+    if (!USERNAME || !USER_PASSWORD || !USER_EMAIL) {
+      console.error('Please define USERNAME, USER_PASSWORD and USER_EMAIL in the .env file');
       return;
     }
 
@@ -19,6 +19,7 @@ module.exports = {
       {
         username: USERNAME,
         password_hash: hashedPassword,
+        email: USER_EMAIL,
         createdAt: new Date(),
         updatedAt: new Date(),
       },
